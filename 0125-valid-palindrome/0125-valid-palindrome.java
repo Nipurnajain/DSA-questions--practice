8fc8028a89581585df43aba614dc0eb42ae01c47
Nipurna Bandi <88611376+Nipurnajain@users.noreply.github.com>
2026-02-19 18:41:15 +0530

    Time: 2 ms (98.99%), Space: 44.5 MB (67.16%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -7,7 +7,7 @@ class Solution {
                 left++;
             }
             while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
-                right--;
+               right--;
             }
             if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
                 return false;
